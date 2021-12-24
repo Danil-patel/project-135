0590335923a6242a57ade7984a589e51c73c6181
Danil-patel <79022114+Danil-patel@users.noreply.github.com>
2021-12-24 16:40:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+objects = [];
+myStatus= "";
+function preload() {
+
+}
+
+function draw() {
+    image(video,0,0,480,380);
+}
+
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480,380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = 'status: detecting objects';
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded() {
+    console.log('model loaded');
+    myStatus = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
