b424db812b5975bdf9bb006b731d4f599ffd90e3
Danil-patel <79022114+Danil-patel@users.noreply.github.com>
2021-12-28 16:36:54 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ function preload() {
 
 function draw() {
     image(video,0,0,480,380);
+    if(myStatus!='') {
+        objectDetector.detect(video,gotResult);
+        for(i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = 'status: object detected';
+            document.getElementById("number_of_objects").innerHTML = 'number of objects detected are: ' + objects.length;
+
+            fill("#ff0000");
+            stroke("#ff0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + ' ' + percent + '%', objects[i].x+15, objects[i].y+15);
+            noFill();
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function setup() {
@@ -28,4 +42,14 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error,results) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
 }
